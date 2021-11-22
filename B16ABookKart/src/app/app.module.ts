f43e117e2f9a44659fb2ef8e52a3d32f42a067c3
maggie280 <shreemegha295@gmail.com>
2021-11-22 16:44:38 +0530

    commit

--- B16ABookKart/src/app/app.module.ts
+++ B16ABookKart/src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BookComponent } from './book/book.component';
+import { SimilarBooksComponent } from './book/similar-books/similar-books.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BookComponent,
+    SimilarBooksComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
